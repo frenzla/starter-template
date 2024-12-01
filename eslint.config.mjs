0abca4e2aad95f079c7dea72c4c60d4c0facd580
Frenzla <86185774+frenzla@users.noreply.github.com>
2024-12-01 22:13:20 +0100

    template v1.1-20241201

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   eslintPluginPrettier,
 ];
